5965f9916b3605c73ce3ce9b517916e8a422556b
AAYUSH KUMAR <aayushrocksall@gmail.com>
2021-06-05 12:27:46 +0530

    Implemented JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
@@ -46,7 +48,23 @@ router.post(
 
       await user.save();
 
-      res.send("passed");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Server Error");
