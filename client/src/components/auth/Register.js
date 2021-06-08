56c6f089da6c3508a1e0079608f056bd3aa94650
AAYUSH KUMAR <aayushrocksall@gmail.com>
2021-06-08 17:54:46 +0530

    Add Login And Register Route

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = user;
+
+  const onChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            name='name'
+            value={name}
+            placeholder='Enter your name'
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            placeholder='Enter your email'
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            placeholder='Enter your password'
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password2'>Confirm Password</label>
+          <input
+            type='password'
+            name='password2'
+            value={password2}
+            placeholder='Confirm Password'
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
