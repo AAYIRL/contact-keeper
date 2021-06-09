b2d99d3c3d201ddda220b1675a759d0a21b30576
AAYUSH KUMAR <aayushrocksall@gmail.com>
2021-06-09 22:08:55 +0530

    Update Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -85,6 +85,39 @@ const ContactState = (props) => {
     }
   };
 
+  //Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({
+        type: UPDATE_CONTACTS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
+  //Filter Contact
+  const filterContact = (text) => {
+    dispatch({
+      type: FILTER_CONTACTS,
+      payload: text,
+    });
+  };
   //Clear contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -101,20 +134,6 @@ const ContactState = (props) => {
   const clearCurrent = () => {
     dispatch({
       type: CLEAR_CURRENT,
-    });
-  };
-  //Update Contact
-  const updateContact = (contact) => {
-    dispatch({
-      type: UPDATE_CONTACTS,
-      payload: contact,
-    });
-  };
-  //Filter Contact
-  const filterContact = (text) => {
-    dispatch({
-      type: FILTER_CONTACTS,
-      payload: text,
     });
   };
 
